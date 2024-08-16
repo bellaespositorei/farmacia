9d20d0e65971709087ddfed5e11bcb661e271c40
bellaespositorei <contato.isabellaesposito@gmail.com>
2024-08-16 12:25:28 -0300

    :sparkles: feat: CRUD Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import "./App.css";
-import Home from "./pages/home/Home";
+
 import Navbar from "./components/navBar/NavBar";
 import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import ListarCategorias from "./components/categorias/listarCategorias/ListarCategorias";
+import FormCategoria from "./components/categorias/formCategoria/FormCategoria";
+import DeletarCategoria from "./components/deletarCategoria/DeletarCategoria";
 
 function App() {
   return (
@@ -11,8 +15,14 @@ function App() {
         <Navbar />
         <div className="min-h-[80vh]">
           <Routes>
-            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/categorias" element={<ListarCategorias />} />
+            <Route path="/cadastroCategoria" element={<FormCategoria />} />
+            <Route path="/editarCategoria/:id" element={<FormCategoria />} />
+            <Route
+              path="/deletarCategoria/:id"
+              element={<DeletarCategoria />}
+            />
           </Routes>
         </div>
         <Footer />
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,19 +1,22 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
       <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
         <div className="container flex justify-between text-lg">
-          <div className="text-2xl font-bold uppercase">Farmácia</div>
+          <Link to="/home" className="text-2xl font-bold uppercase">
+            Farmácia
+          </Link>
 
           <div className="flex gap-4">
-            <Link to="/home" className="hover:underline">
-              Home
-            </Link>
             <div className="hover:underline">Produtos</div>
-            <div className="hover:underline">Categoria</div>
-            <div className="hover:underline">Cadastrar categoria</div>
+            <Link to="/categorias" className="hover:underline">
+              Categorias
+            </Link>
+            <Link to="/cadastroCategoria" className="hover:underline">
+              Cadastrar Categoria
+            </Link>
           </div>
         </div>
       </div>
